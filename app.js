8353aad7f951e87250388e192c1267c299c29019
Hellerin <hellerinm@gmail.com>
2021-10-14 09:02:08 +0300

    Teksti väljastamine konsool ja kommenteerimine

---
+++ app.js
@@ -0,0 +1,14 @@
+// teksti väljastamine
+// kommentaaride väljastamine
+
+// üherealine kommentaar
+
+/*
+Mitme
+realine
+kommentaar
+ */
+console.log("Tere Maailm")
+console.log('See on teine rida')
+
+console.log('See ' + 'on ' + 'liitluase')
